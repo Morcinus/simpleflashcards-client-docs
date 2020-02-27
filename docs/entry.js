844791bff1769b3db1e867263dd6fa380f72d91e
Morcinus <30431494+Morcinus@users.noreply.github.com>
2020-02-27 18:49:32 +0100

    Add Docs

---
+++ docs/entry.js
@@ -0,0 +1,26 @@
+
+    window.reactComponents = {};
+
+    window.vueComponents = {};
+
+  
+      import React from "react";
+
+      import ReactDOM from "react-dom";
+
+
+      import ReactWrapper from '../../C:\Morcinus Files\Google Drive\Projects\App SimpleFlashCards (maturita)\SimpleFlashCards\simpleflashcards-client\node_modules\better-docs/lib/react-wrapper.js';
+
+      window.React = React;
+
+      window.ReactDOM = ReactDOM;
+
+      window.ReactWrapper = ReactWrapper;
+
+    
+    import './styles/reset.css';
+
+    import './styles/iframe.css';
+
+  import Component0 from '../../C:\Morcinus Files\Google Drive\Projects\App SimpleFlashCards (maturita)\SimpleFlashCards\simpleflashcards-client\src\components\CollectionDecks.js';
+reactComponents['CollectionDecks'] = Component0;
